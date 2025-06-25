408925eebf52f0444b93bcea4f478b11d4c46fad
Poori Charicha <phuri.c@kkumail.com>
2025-06-25 15:55:15 +0700

    add field name

--- Student.java
+++ Student.java
@@ -2,4 +2,8 @@ class Student{
     public Stundent(){
         //additional initialization code can go here
     }
+    private String name;
+    public String getName() {
+        return this.name;
+    }
 }
