b14c1feb8166c93d1bdd5d9cec32324d94bf1069
Poori Charicha <phuri.c@kkumail.com>
2025-06-25 16:30:26 +0700

    add Teacher class

---
+++ Teacher.java
@@ -0,0 +1,25 @@
+publci class Teacher {
+    private String name;
+    private String subject;
+
+    public Teacher(String name, String subject) {
+        this.name = name;
+        this.subject = subject;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    @Override
+    public String toString() {
+        return "Teacher{" +
+                "name='" + name + '\'' +
+                ", subject='" + subject + '\'' +
+                '}';
+    }
+}
